457eb7f90d313122dead5cab40c41b3aa3751c8f
Sujani <sujaninalla11@gmail.com>
2025-07-15 07:37:41 -0400

    E2E Api Flow

---
+++ RestAssured/src/test/java/E2E_API_FLOW/Login_Request_POJOClass_E2E.java
@@ -0,0 +1,21 @@
+package E2E_API_FLOW;
+
+public class Login_Request_POJOClass_E2E {
+
+	private String userEmail;
+	private String userPassword;
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	
+}
